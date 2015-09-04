8bab8acfd2674e08765517a3dfa7e6a5aaeaaa62
tharindu <tharindu.prf@gmail.com>
2015-09-04 15:43:04 +0530

    initial commit

---
+++ template/routes/api.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var songController = require('../controllers/song');
+
+router.route('/songs')
+
+	.post(function(req, res) {
+	songController.save(req, res)
+	})
+	.get(songController.see);
+
+router.route('/songs/:songId')
+
+	.put(function(req, res) {
+	songController.updatesongId(req, res)
+	})
+	.get(songController.getsongId)
+	.delete(function(req, res) {
+	songController.deletesongId(req, res)
+	});
+
+router.route('/songs/:name')
+
+	.get(songController.getname);
+
+var artistController = require('../controllers/artist');
+
+router.route('/artists')
+
+	.post(function(req, res) {
+	artistController.save(req, res)
+	})
+	.get(artistController.see);
+
+router.route('/artists/:name')
+
+	.put(function(req, res) {
+	artistController.updatename(req, res)
+	})
+	.get(artistController.getname)
+	.delete(function(req, res) {
+	artistController.deletename(req, res)
+	});
+
+
+
+module.exports = router;
